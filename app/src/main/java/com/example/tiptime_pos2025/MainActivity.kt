db0644ffb470160edc83facbe24e3a564ac2a7a1
alfaenzoh <enzoyutakah@gmail.com>
2025-03-25 11:37:02 -0300

    feat: Tip Project Complete

---
+++ app/src/main/java/com/example/tiptime_pos2025/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.tiptime_pos2025
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime_pos2025.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{
+            calculateTip()
+        }
+
+
+        if(savedInstanceState != null){
+            binding.tipResult.text = savedInstanceState.getString("result")
+
+        }
+    }
+    private fun calculateTip(){
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull() ?: return
+
+        val selectedId = binding.tipOption.checkedRadioButtonId
+
+        val tipPercentage = when(selectedId){
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        if(roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount_s, formattedTip)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle){
+        super.onSaveInstanceState(outState)
+        outState.putString("result", binding.tipResult.text.toString())
+    }
+}
